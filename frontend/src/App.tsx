824fb5a916b6d8abbf828a64f77385712bfac110
Takumikimura1899 <kimura141899@gmail.com>
2024-03-31 20:47:24 +0900

    get csrf_token from front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Auth } from './components/Auth';
 import { Todo } from './components/Todo';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { CsrfToken } from './types';
 
 function App() {
+  useEffect(() => {
+    axios.defaults.withCredentials = true;
+    const getCsrfToken = async () => {
+      const { data } = await axios.get<CsrfToken>(
+        `${import.meta.env.VITE_REACT_APP_API_URL}/csrf`
+      );
+      axios.defaults.headers.common['X-CSRF-Token'] = data.csrf_token;
+    };
+    getCsrfToken();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>

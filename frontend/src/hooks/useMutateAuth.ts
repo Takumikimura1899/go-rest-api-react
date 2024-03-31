d89e72e0ef6a5687a234144e508a0b8432b25370
Takumikimura1899 <kimura141899@gmail.com>
2024-04-01 07:40:28 +0900

    create hooks useMutateAuth

---
+++ frontend/src/hooks/useMutateAuth.ts
@@ -0,0 +1,64 @@
+import { useNavigate } from 'react-router-dom';
+import useStore from '../store';
+import { useError } from './useError';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+
+export const useMutateAuth = () => {
+  const navigate = useNavigate();
+  const resetEditedTask = useStore((state) => state.resetEditedTask);
+  const { switchErrorHandling } = useError();
+
+  const loginMutation = useMutation(
+    async (user: Credential) =>
+      await axios.post(`${import.meta.env.VITE_REACT_APP_API_URL}/login`, user),
+    {
+      onSuccess: () => {
+        navigate('/todo');
+      },
+      onError: (error: any) => {
+        if (error.response.data.message) {
+          switchErrorHandling(error.response.data.message);
+        } else {
+          switchErrorHandling(error.response.data);
+        }
+      },
+    }
+  );
+
+  const registerMutation = useMutation(
+    async (user: Credential) =>
+      await axios.post(
+        `${import.meta.env.VITE_REACT_APP_API_URL}/signup`,
+        user
+      ),
+    {
+      onError: (error: any) => {
+        if (error.response.data.message) {
+          switchErrorHandling(error.response.data.message);
+        } else {
+          switchErrorHandling(error.response.data);
+        }
+      },
+    }
+  );
+
+  const logoutMutation = useMutation(
+    async () =>
+      await axios.post(`${import.meta.env.VITE_REACT_APP_API_URL}/logout`),
+    {
+      onSuccess: () => {
+        resetEditedTask();
+        navigate('/');
+      },
+      onError: (error: any) => {
+        if (error.response.data.message) {
+          switchErrorHandling(error.response.data.message);
+        } else {
+          switchErrorHandling(error.response.data);
+        }
+      },
+    }
+  );
+  return { loginMutation, registerMutation, logoutMutation };
+};
